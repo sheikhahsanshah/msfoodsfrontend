4e1f45a823bacafe813552daa05641e9a212f3ad
husnainalyy <alyhusnaiin@gmail.com>
2025-05-11 08:15:14 +0500

    chnages

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
     reactStrictMode: false,
     images: {
-        domains: ['res.cloudinary.com','https://res.cloudinary.com/'],
+        domains: ['res.cloudinary.com', 'https://res.cloudinary.com/'],
+    },
+    async redirects() {
+        return [
+            {
+                source: '/(.*)',
+                has: [
+                    {
+                        type: 'host',
+                        value: 'www.msfoods.pk',
+                    },
+                ],
+                destination: 'https://msfoods.pk/:path*',
+                permanent: true,
+            },
+        ];
     },
 };
 
